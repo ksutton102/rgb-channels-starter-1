339b702fd1273f5eb87cb1ada8eac296a65f44c5
Khalilah Sutton <ksutton@wlapcs.org>
2018-01-23 19:32:08 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -4,13 +4,24 @@ var lpkidsblue;
 var lpkidsgray;
 
 function preload(){
-    
+    lpkidsgray = loadImage("images/lpkids-gray.png");
+    lpkidsred = loadImage("images/lpkids-red.png");
+    lpkidsblue = loadImage("images/lpkids-blue.png");
+    lpkidsgreen = loadImage("images/lpkids-green.png");
 }
+
 
 function setup(){
     createCanvas(500,374);
 }
 
 function draw(){
-    
+     if(keyIsPressed){
+         if(key=="r"){
+            image(lpkidsred, 0, 0);
+         }
+    }
+    else{
+        image(lpkidsgray, 0, 0);
+    }
 }
