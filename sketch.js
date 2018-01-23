592cacecb10bcb3afd37c4d0e5b96f0c370d424f
Khalilah Sutton <ksutton@wlapcs.org>
2018-01-23 20:18:54 +0000

    third commit

--- sketch.js
+++ sketch.js
@@ -14,35 +14,34 @@ function preload(){
 
 
 function setup(){
-    createCanvas(500,374);
+    createCanvas(749,524);
 }
 
 function draw(){
      if(keyIsPressed){
          if(key=="r"){
-            image(lpkidsred, 0, 0);
+            image(lpkidsred, 50, 50);
          }
          if(key=="b"){
-            image(lpkidsblue, 0, 0);
+            image(lpkidsblue, 50, 50);
          }
          
          if(key=="g"){
-            image(lpkidsgreen, 0, 0);
+            image(lpkidsgreen, 50, 50);
           }
           
          if(keyCode==32){
              fill(0);
-             rect(0, 0, 500, 374);
+             rect(50, 50, 500, 374);
              blendMode(LIGHTEST);
-             image(lpkidsred, 0, 0);
-             image(lpkidsgreen, 0, 0);
-             image(lpkidsblue, 0, 0);
+             image(lpkidsred, 50, 50);
+             image(lpkidsgreen, 50, 50);
+             image(lpkidsblue, 50, 50);
          }
     }
         else{
-            blendMode(NORMAL);
-            image(lpkidsgray, 0, 0);
-            image(TV, 0, 0);
+            image(lpkidsgray, 50, 50);
         }
-    
+            blendMode(NORMAL);
+            image(TV, 0, 0);
 }
